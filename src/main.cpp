814bf0688b908ba807020fa26cc9141b206c82f6
Isaac Poole <isaacpoolemusic@gmail.com>
2021-11-23 09:14:34 -0500

    sets up snare and rim

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,38 @@
 #include <Arduino.h>
+#include <hellodrum.h>
+
+byte SNARE[9] = {
+    90, //sensitivity (1-100)
+    50,  //threshold (1-100)
+    30,  //scan time (1-100)
+    10,  //mask time (1-100)
+    10,  //rim sensitivity (1-100)
+    3,   //rim threshold (1-100)
+    3,   //curve type (0-4)
+    38,  //note (0-127)
+    37   //note of cross stick (0-127)
+};
+
+HelloDrum snare(10, 11);
 
 void setup() {
-  // put your setup code here, to run once:
+  usbMIDI.begin();
+  Serial.begin(38400);
+
+  snare.setCurve(SNARE[6]);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+    snare.dualPiezo(SNARE[0], SNARE[1], SNARE[2], SNARE[3], SNARE[4], SNARE[5]);
+
+  if (snare.hit == true)
+  {
+    usbMIDI.sendNoteOn(SNARE[7], snare.velocity, 10); //(note, velocity, channel)
+    usbMIDI.sendNoteOff(SNARE[7], 0, 10);
+  }
+  else if (snare.hitRim == true)
+  {
+      usbMIDI.sendNoteOn(SNARE[8], snare.velocity, 10); //(note, velocity, channel)
+      usbMIDI.sendNoteOff(SNARE[8], 0, 10);
+  }
 }
