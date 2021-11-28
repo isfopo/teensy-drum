a0d2017578604bee5b2aa8c05ca9b2cc1b2255f5
Isaac Poole <isaacpoolemusic@gmail.com>
2021-11-27 21:14:42 -0500

    formats

--- src/main.cpp
+++ src/main.cpp
@@ -14,14 +14,14 @@ byte KICK[6] = {
 
 byte SNARE[9] = {
     90, //sensitivity (1-100)
-    50,  //threshold (1-100)
-    30,  //scan time (1-100)
-    10,  //mask time (1-100)
-    10,  //rim sensitivity (1-100)
-    3,   //rim threshold (1-100)
-    3,   //curve type (0-4)
-    38,  //note (0-127)
-    37   //note of cross stick (0-127)
+    50, //threshold (1-100)
+    30, //scan time (1-100)
+    10, //mask time (1-100)
+    10, //rim sensitivity (1-100)
+    3,  //rim threshold (1-100)
+    3,  //curve type (0-4)
+    38, //note (0-127)
+    37  //note of cross stick (0-127)
 };
 
 byte HIHAT[6] = {
@@ -41,17 +41,20 @@ HelloDrum kick(12);
 HelloDrum snare(10, 11);
 HelloDrum hihat(1);
 
-void setup() {
+void setup()
+{
   kick.setCurve(KICK[5]);
   snare.setCurve(SNARE[6]);
 }
 
-void loop() {
+void loop()
+{
   kick.singlePiezo(KICK[0], KICK[1], KICK[2], KICK[3]);
   snare.dualPiezo(SNARE[0], SNARE[1], SNARE[2], SNARE[3], SNARE[4], SNARE[5]);
   hihat.HH(HIHAT[0], HIHAT[1], HIHAT[2], HIHAT[3]);
 
-  if (kick.hit == true) {
+  if (kick.hit == true)
+  {
     usbMIDI.sendNoteOn(KICK[4], kick.velocity, MIDI_CHANNEL); //(note, velocity, channel)
     usbMIDI.sendNoteOff(KICK[4], 0, MIDI_CHANNEL);
   }
@@ -63,8 +66,8 @@ void loop() {
   }
   else if (snare.hitRim == true)
   {
-      usbMIDI.sendNoteOn(SNARE[8], snare.velocity, MIDI_CHANNEL); //(note, velocity, channel)
-      usbMIDI.sendNoteOff(SNARE[8], 0, MIDI_CHANNEL);
+    usbMIDI.sendNoteOn(SNARE[8], snare.velocity, MIDI_CHANNEL); //(note, velocity, channel)
+    usbMIDI.sendNoteOff(SNARE[8], 0, MIDI_CHANNEL);
   }
 
   if (hihat.hit == true && !snare.hitRim)
