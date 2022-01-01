5c670fc5acf6bdcc16324afd06fe334ccbe88f7f
Isaac Poole <isaacpoolemusic@gmail.com>
2021-12-31 19:54:52 -0500

    separates and simplifies pad notes and settings

--- src/main.cpp
+++ src/main.cpp
@@ -44,82 +44,30 @@ byte HIHAT_PEDAL[7] = {
     100, //note threshold
 };
 
-byte PAD_1[6]{
+byte PADS[5]{
     100, //sensitivity (1-100)
     50,  //threshold (1-100)
     10,  //scan time (1-)
     200,  //mask time (1-)
-    41,  //note (0-127)
     1,   //curve type (0-4)
 };
 
-byte PAD_2[6]{
-    10, //sensitivity (1-100)
-    50,  //threshold (1-100)
-    50,  //scan time (1-)
-    100,  //mask time (1-)
-    43,  //note (0-127)
-    4,   //curve type (0-4)
-};
-
-byte PAD_3[6]{
-    100, //sensitivity (1-100)
-    50,  //threshold (1-100)
-    10,  //scan time (1-)
-    200,  //mask time (1-)
-    45,  //note (0-127)
-    1,   //curve type (0-4)
-};
-
-byte PAD_4[6]{
-    100, //sensitivity (1-100)
-    50,  //threshold (1-100)
-    10,  //scan time (1-)
-    200,  //mask time (1-)
-    47,  //note (0-127)
-    1,   //curve type (0-4)
-};
-
-byte PAD_5[6]{
-    100, //sensitivity (1-100)
-    50,  //threshold (1-100)
-    10,  //scan time (1-)
-    200,  //mask time (1-)
-    48,  //note (0-127)
-    1,   //curve type (0-4)
-};
-
-byte PAD_6[6]{
-    100, //sensitivity (1-100)
-    50,  //threshold (1-100)
-    10,  //scan time (1-)
-    200,  //mask time (1-)
-    49,  //note (0-127)
-    1,   //curve type (0-4)
-};
-
-byte PAD_7[6]{
-    100, //sensitivity (1-100)
-    50,  //threshold (1-100)
-    10,  //scan time (1-)
-    200,  //mask time (1-)
-    50,  //note (0-127)
-    1,   //curve type (0-4)
-};
-
-byte PAD_8[6]{
-    100, //sensitivity (1-100)
-    50,  //threshold (1-100)
-    10,  //scan time (1-)
-    200,  //mask time (1-)
-    51,  //note (0-127)
-    1,   //curve type (0-4)
+byte PAD_NOTES[8] {
+  41,
+  43,
+  45,
+  47,
+  48,
+  49,
+  50,
+  51,
 };
 
 HelloDrum kick(12);
 HelloDrum snare(11, 10);
 HelloDrum hihat(8);
 HelloDrum hihatPedal(9);
+
 HelloDrum pad1(0);
 HelloDrum pad2(1);
 HelloDrum pad3(2);
@@ -134,14 +82,14 @@ void setup()
   kick.setCurve(KICK[5]);
   snare.setCurve(SNARE[6]);
   hihat.setCurve(HIHAT[6]);
-  pad1.setCurve(PAD_1[5]);
-  pad2.setCurve(PAD_2[5]);
-  pad3.setCurve(PAD_3[5]);
-  pad4.setCurve(PAD_4[5]);
-  pad5.setCurve(PAD_5[5]);
-  pad6.setCurve(PAD_6[5]);
-  pad7.setCurve(PAD_7[5]);
-  pad8.setCurve(PAD_8[5]);
+  pad1.setCurve(PADS[4]);
+  pad2.setCurve(PADS[4]);
+  pad3.setCurve(PADS[4]);
+  pad4.setCurve(PADS[4]);
+  pad5.setCurve(PADS[4]);
+  pad6.setCurve(PADS[4]);
+  pad7.setCurve(PADS[4]);
+  pad8.setCurve(PADS[4]);
 }
 
 void loop()
@@ -150,14 +98,14 @@ void loop()
   snare.dualPiezo(SNARE[0], SNARE[1], SNARE[2], SNARE[3], SNARE[4], SNARE[5]);
   hihat.HH(HIHAT[0], HIHAT[1], HIHAT[2], HIHAT[3]);
   hihatPedal.hihatControl(HIHAT_PEDAL[0], HIHAT_PEDAL[1], HIHAT_PEDAL[2], HIHAT_PEDAL[3], HIHAT_PEDAL[4]);
-  pad1.singlePiezo(PAD_1[0], PAD_1[1], PAD_1[2], PAD_1[3]);
-  pad2.singlePiezo(PAD_2[0], PAD_2[1], PAD_2[2], PAD_2[3]);
-  pad3.singlePiezo(PAD_3[0], PAD_3[1], PAD_3[2], PAD_3[3]);
-  pad4.singlePiezo(PAD_4[0], PAD_4[1], PAD_4[2], PAD_4[3]);
-  pad5.singlePiezo(PAD_5[0], PAD_5[1], PAD_5[2], PAD_5[3]);
-  pad6.singlePiezo(PAD_6[0], PAD_6[1], PAD_6[2], PAD_6[3]);
-  pad7.singlePiezo(PAD_7[0], PAD_7[1], PAD_7[2], PAD_7[3]);
-  pad8.singlePiezo(PAD_8[0], PAD_8[1], PAD_8[2], PAD_8[3]);
+  pad1.singlePiezo(PADS[0], PADS[1], PADS[2], PADS[3]);
+  pad2.singlePiezo(PADS[0], PADS[1], PADS[2], PADS[3]);
+  pad3.singlePiezo(PADS[0], PADS[1], PADS[2], PADS[3]);
+  pad4.singlePiezo(PADS[0], PADS[1], PADS[2], PADS[3]);
+  pad5.singlePiezo(PADS[0], PADS[1], PADS[2], PADS[3]);
+  pad6.singlePiezo(PADS[0], PADS[1], PADS[2], PADS[3]);
+  pad7.singlePiezo(PADS[0], PADS[1], PADS[2], PADS[3]);
+  pad8.singlePiezo(PADS[0], PADS[1], PADS[2], PADS[3]);
 
   // Kick
 
@@ -211,49 +159,49 @@ void loop()
 
   if (pad1.hit == true)
   {
-    usbMIDI.sendNoteOn(PAD_1[4], pad1.velocity, MIDI_CHANNEL); //(note, velocity, channel)
-    usbMIDI.sendNoteOff(PAD_1[4], 0, MIDI_CHANNEL);
+    usbMIDI.sendNoteOn(PAD_NOTES[0], pad1.velocity, MIDI_CHANNEL); //(note, velocity, channel)
+    usbMIDI.sendNoteOff(PAD_NOTES[0], 0, MIDI_CHANNEL);
   }
 
   else if (pad2.hit == true)
   {
-    usbMIDI.sendNoteOn(PAD_2[4], pad1.velocity, MIDI_CHANNEL); //(note, velocity, channel)
-    usbMIDI.sendNoteOff(PAD_2[4], 0, MIDI_CHANNEL);
+    usbMIDI.sendNoteOn(PAD_NOTES[1], pad1.velocity, MIDI_CHANNEL); //(note, velocity, channel)
+    usbMIDI.sendNoteOff(PAD_NOTES[1], 0, MIDI_CHANNEL);
   }
 
   else if (pad3.hit == true)
   {
-    usbMIDI.sendNoteOn(PAD_3[4], pad1.velocity, MIDI_CHANNEL); //(note, velocity, channel)
-    usbMIDI.sendNoteOff(PAD_3[4], 0, MIDI_CHANNEL);
+    usbMIDI.sendNoteOn(PAD_NOTES[2], pad1.velocity, MIDI_CHANNEL); //(note, velocity, channel)
+    usbMIDI.sendNoteOff(PAD_NOTES[2], 0, MIDI_CHANNEL);
   }
 
   else if (pad4.hit == true)
   {
-    usbMIDI.sendNoteOn(PAD_4[4], pad1.velocity, MIDI_CHANNEL); //(note, velocity, channel)
-    usbMIDI.sendNoteOff(PAD_4[4], 0, MIDI_CHANNEL);
+    usbMIDI.sendNoteOn(PAD_NOTES[3], pad1.velocity, MIDI_CHANNEL); //(note, velocity, channel)
+    usbMIDI.sendNoteOff(PAD_NOTES[3], 0, MIDI_CHANNEL);
   }
 
   else if (pad5.hit == true)
   {
-    usbMIDI.sendNoteOn(PAD_5[4], pad1.velocity, MIDI_CHANNEL); //(note, velocity, channel)
-    usbMIDI.sendNoteOff(PAD_5[4], 0, MIDI_CHANNEL);
+    usbMIDI.sendNoteOn(PAD_NOTES[4], pad1.velocity, MIDI_CHANNEL); //(note, velocity, channel)
+    usbMIDI.sendNoteOff(PAD_NOTES[4], 0, MIDI_CHANNEL);
   }
 
   else if (pad6.hit == true)
   {
-    usbMIDI.sendNoteOn(PAD_6[4], pad1.velocity, MIDI_CHANNEL); //(note, velocity, channel)
-    usbMIDI.sendNoteOff(PAD_6[4], 0, MIDI_CHANNEL);
+    usbMIDI.sendNoteOn(PAD_NOTES[5], pad1.velocity, MIDI_CHANNEL); //(note, velocity, channel)
+    usbMIDI.sendNoteOff(PAD_NOTES[5], 0, MIDI_CHANNEL);
   }
 
   else if (pad7.hit == true)
   {
-    usbMIDI.sendNoteOn(PAD_7[4], pad1.velocity, MIDI_CHANNEL); //(note, velocity, channel)
-    usbMIDI.sendNoteOff(PAD_7[4], 0, MIDI_CHANNEL);
+    usbMIDI.sendNoteOn(PAD_NOTES[6], pad1.velocity, MIDI_CHANNEL); //(note, velocity, channel)
+    usbMIDI.sendNoteOff(PAD_NOTES[6], 0, MIDI_CHANNEL);
   }
 
   else if (pad8.hit == true)
   {
-    usbMIDI.sendNoteOn(PAD_8[4], pad1.velocity, MIDI_CHANNEL); //(note, velocity, channel)
-    usbMIDI.sendNoteOff(PAD_8[4], 0, MIDI_CHANNEL);
+    usbMIDI.sendNoteOn(PAD_NOTES[7], pad1.velocity, MIDI_CHANNEL); //(note, velocity, channel)
+    usbMIDI.sendNoteOff(PAD_NOTES[7], 0, MIDI_CHANNEL);
   }
 }
